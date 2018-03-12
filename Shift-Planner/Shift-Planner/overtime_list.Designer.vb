886a779560ae36bd5f7aed8488e89866eecc9712
UP807742 <up807742@myport.ac.uk>
2018-03-12 19:37:11 +0000

    ListBox now updates Text field with overtime shift details
    Overtime shifts from the database can now be selected and applied for with their details being shown in a text box

--- Shift-Planner/Shift-Planner/overtime_list.Designer.vb
+++ Shift-Planner/Shift-Planner/overtime_list.Designer.vb
@@ -22,87 +22,86 @@ Partial Class overtime_list
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.ListBox1 = New System.Windows.Forms.ListBox()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.txtEmployeeID = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
-        '
-        'ComboBox1
-        '
-        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Items.AddRange(New Object() {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"})
-        Me.ComboBox1.Location = New System.Drawing.Point(214, 17)
-        Me.ComboBox1.Margin = New System.Windows.Forms.Padding(2)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(92, 21)
-        Me.ComboBox1.TabIndex = 0
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(10, 17)
-        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Location = New System.Drawing.Point(13, 21)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(207, 20)
+        Me.Label1.Size = New System.Drawing.Size(236, 25)
         Me.Label1.TabIndex = 2
-        Me.Label1.Text = "Available Overtime Shifts for"
+        Me.Label1.Text = "Available Overtime Shifts:"
         '
         'ListBox1
         '
         Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.Location = New System.Drawing.Point(14, 69)
-        Me.ListBox1.Margin = New System.Windows.Forms.Padding(2)
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(19, 85)
+        Me.ListBox1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.ListBox1.MultiColumn = True
         Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(345, 524)
+        Me.ListBox1.Size = New System.Drawing.Size(459, 644)
         Me.ListBox1.TabIndex = 4
         '
         'TextBox1
         '
         Me.TextBox1.BackColor = System.Drawing.SystemColors.Control
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.TextBox1.Enabled = False
-        Me.TextBox1.Location = New System.Drawing.Point(410, 69)
-        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2)
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(547, 85)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.TextBox1.Multiline = True
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(365, 280)
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(485, 279)
         Me.TextBox1.TabIndex = 5
         '
         'txtEmployeeID
         '
         Me.txtEmployeeID.AutoSize = True
-        Me.txtEmployeeID.Location = New System.Drawing.Point(714, 17)
-        Me.txtEmployeeID.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.txtEmployeeID.Location = New System.Drawing.Point(952, 21)
         Me.txtEmployeeID.Name = "txtEmployeeID"
-        Me.txtEmployeeID.Size = New System.Drawing.Size(10, 13)
+        Me.txtEmployeeID.Size = New System.Drawing.Size(12, 17)
         Me.txtEmployeeID.TabIndex = 6
         Me.txtEmployeeID.Text = " "
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(560, 312)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(121, 41)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "Apply for shift"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'overtime_list
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(805, 635)
+        Me.ClientSize = New System.Drawing.Size(1073, 782)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.txtEmployeeID)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.ComboBox1)
-        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "overtime_list"
         Me.Text = "Overtime Shifts"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents ComboBox1 As ComboBox
     Friend WithEvents Label1 As Label
     Friend WithEvents ListBox1 As ListBox
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents txtEmployeeID As Label
+    Friend WithEvents Button1 As Button
 End Class
--- Shift-Planner/Shift-Planner/overtime_list.vb
+++ Shift-Planner/Shift-Planner/overtime_list.vb
@@ -2,15 +2,19 @@
 
     Public sqlQuery As String
     Public i As Integer
-
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
-
-    End Sub
+    Public currentUser As Integer
+    Public otType As String
+    Public otDate As String
+    Public otStart As String
+    Public otEnd As String
+    Public otPay As Decimal
 
     Private Sub Overtime_list_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'Gets the employee ID of the user that is currently logged on
+        'currentUser = ds.Tables("tblLogOn").Rows(0).Item("employeeID")
+
         DBConnect()
-
-
 
         sqlQuery = "SELECT overtimeDate, overtimePay FROM otTest"
 
@@ -19,24 +23,34 @@
         con.Close()
 
         For i = 0 To ds.Tables("tblOvertime").Rows.Count() - 1
-
-
-            ListBox1.Items.Add(ds.Tables("tblOvertime").Rows(i).Item("overtimeDate") + "    " + ds.Tables("tblOvertime").Rows(i).Item("overtimePay"))
+            ListBox1.Items.Add("Date:   " + ds.Tables("tblOvertime").Rows(i).Item("overtimeDate") + "    " + "Pay:  " + ds.Tables("tblOvertime").Rows(i).Item("overtimePay") + " p/h" + ds.Tables("tblOvertime").Rows(i).Item("overtimeType") + "Start: " + ds.Tables("tblOvertime").Rows(i).Item("overtimeStart") + "Finish:   " + ds.Tables("tblOvertime").Rows(i).Item("overtimeEnd"))
         Next
     End Sub
 
     Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
 
+        otType = ds.Tables("tblOvertime").Rows(ListBox1.SelectedIndex).Item("overtimeType")
+        otDate = ds.Tables("tblOvertime").Rows(ListBox1.SelectedIndex).Item("overtimeDate")
+        otStart = ds.Tables("tblOvertime").Rows(ListBox1.SelectedIndex).Item("overtimeStart")
+        otEnd = ds.Tables("tblOvertime").Rows(ListBox1.SelectedIndex).Item("overtimeEnd")
+        otPay = ds.Tables("tblOvertime").Rows(ListBox1.SelectedIndex).Item("overtimePay")
 
-        sqlQuery = "SELECT overtimeDate, overtimePay FROM OVERTIME"
-
-        da = New OleDb.OleDbDataAdapter(sqlQuery, con)
-        da.Fill(ds, "tblOvertime")
-        con.Close()
-
-
-        'For Each row In ds.Tables("tblOvertime")
-        'ListBox1.Items.Add(New ListItem(ds.Tables("tblOvertime").Rows(row).Item("overtimeDate"), ds.Tables("tblOvertime").Rows(row).Item("overtimePay")))
-        'Next
+        TextBox1.Text = "Would you like to apply for the following shift? " & Environment.NewLine & "Position:  " & otType & "Date:  " & otDate & "Start Time:  " & otStart & "End Time:  " & otEnd & "Pay (per hour):  " & otPay
     End Sub
+
+    'Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    '    sqlQuery = "INSERT INTO tblovertime (employeeID, overtimeType, overtimeDate, overTimePay, overtimeStart, overtimeEnd) VALUES ("
+    '    sql& = "'" & currentUser & "',"
+    '    sql& = "'" & otType & "',"
+    '    sql& = "'" & otDate & "',"
+    '    sql& = "'" & otStart & "',"
+    '    sql& = "'" & otEnd & "',"
+    '    sql& = "'" & otPay & "')"
+
+
+    '    da = New OleDb.OleDbDataAdapter(sqlQuery, con)
+    '    da.Fill(ds, "tblOvertime")
+    '    con.Close()
+    'End Sub
 End Class
