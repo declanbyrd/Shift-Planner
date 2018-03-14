2cea93a998efccddc493395ad6e81126d529ea5c
up786633 <36266234+up786633@users.noreply.github.com>
2018-03-14 17:24:31 +0000

    upload holiday form

---
+++ Shift-Planner/Holiday.Designer.vb
@@ -0,0 +1,201 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Holiday
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtEmployeeID = New System.Windows.Forms.TextBox()
+        Me.dtStart = New System.Windows.Forms.DateTimePicker()
+        Me.dtEnd = New System.Windows.Forms.DateTimePicker()
+        Me.txtReason = New System.Windows.Forms.TextBox()
+        Me.lstBoxAccHoll = New System.Windows.Forms.ListBox()
+        Me.lstBoxPendHol = New System.Windows.Forms.ListBox()
+        Me.btnAddTimeOff = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Employee ID:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(7, 44)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(75, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Time Off Start:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(7, 77)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Time Off End:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(32, 114)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(47, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Reason:"
+        '
+        'txtEmployeeID
+        '
+        Me.txtEmployeeID.Location = New System.Drawing.Point(88, 6)
+        Me.txtEmployeeID.Name = "txtEmployeeID"
+        Me.txtEmployeeID.ReadOnly = True
+        Me.txtEmployeeID.Size = New System.Drawing.Size(182, 20)
+        Me.txtEmployeeID.TabIndex = 4
+        '
+        'dtStart
+        '
+        Me.dtStart.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtStart.Location = New System.Drawing.Point(88, 40)
+        Me.dtStart.Name = "dtStart"
+        Me.dtStart.Size = New System.Drawing.Size(182, 20)
+        Me.dtStart.TabIndex = 5
+        '
+        'dtEnd
+        '
+        Me.dtEnd.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtEnd.Location = New System.Drawing.Point(88, 74)
+        Me.dtEnd.Name = "dtEnd"
+        Me.dtEnd.Size = New System.Drawing.Size(182, 20)
+        Me.dtEnd.TabIndex = 6
+        '
+        'txtReason
+        '
+        Me.txtReason.Location = New System.Drawing.Point(88, 110)
+        Me.txtReason.Multiline = True
+        Me.txtReason.Name = "txtReason"
+        Me.txtReason.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtReason.Size = New System.Drawing.Size(182, 71)
+        Me.txtReason.TabIndex = 7
+        '
+        'lstBoxAccHoll
+        '
+        Me.lstBoxAccHoll.FormattingEnabled = True
+        Me.lstBoxAccHoll.Location = New System.Drawing.Point(309, 29)
+        Me.lstBoxAccHoll.Name = "lstBoxAccHoll"
+        Me.lstBoxAccHoll.Size = New System.Drawing.Size(422, 186)
+        Me.lstBoxAccHoll.TabIndex = 8
+        '
+        'lstBoxPendHol
+        '
+        Me.lstBoxPendHol.FormattingEnabled = True
+        Me.lstBoxPendHol.Location = New System.Drawing.Point(309, 248)
+        Me.lstBoxPendHol.Name = "lstBoxPendHol"
+        Me.lstBoxPendHol.Size = New System.Drawing.Size(422, 186)
+        Me.lstBoxPendHol.TabIndex = 9
+        '
+        'btnAddTimeOff
+        '
+        Me.btnAddTimeOff.Location = New System.Drawing.Point(195, 187)
+        Me.btnAddTimeOff.Name = "btnAddTimeOff"
+        Me.btnAddTimeOff.Size = New System.Drawing.Size(75, 23)
+        Me.btnAddTimeOff.TabIndex = 10
+        Me.btnAddTimeOff.Text = "Apply"
+        Me.btnAddTimeOff.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(307, 8)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(99, 13)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Accepted Holidays:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(307, 226)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(92, 13)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Pending Holidays:"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Panel1.Location = New System.Drawing.Point(290, 3)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1, 437)
+        Me.Panel1.TabIndex = 13
+        '
+        'Holiday
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(741, 442)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.btnAddTimeOff)
+        Me.Controls.Add(Me.lstBoxPendHol)
+        Me.Controls.Add(Me.lstBoxAccHoll)
+        Me.Controls.Add(Me.txtReason)
+        Me.Controls.Add(Me.dtEnd)
+        Me.Controls.Add(Me.dtStart)
+        Me.Controls.Add(Me.txtEmployeeID)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Holiday"
+        Me.Text = "Holiday"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtEmployeeID As TextBox
+    Friend WithEvents dtStart As DateTimePicker
+    Friend WithEvents dtEnd As DateTimePicker
+    Friend WithEvents txtReason As TextBox
+    Friend WithEvents lstBoxAccHoll As ListBox
+    Friend WithEvents lstBoxPendHol As ListBox
+    Friend WithEvents btnAddTimeOff As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Panel1 As Panel
+End Class
---
+++ Shift-Planner/Holiday.vb
@@ -0,0 +1,71 @@
+﻿' Holiday class - this form allows employees to
+' apply for time off and see any pending and
+' aproved time off.
+Public Class Holiday
+    ' Form load function, will connect to the database,
+    ' take the employeeID of the currently logged in
+    ' employee and store it in a variable. Will then
+    ' populate 2 list boxes with timeoff data from the db
+    ' for that particular employee.
+    Private Sub Holiday_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim employeeID As Integer
+
+        'Gets the employee ID of the user that Is currently logged on
+
+        employeeID = ds.Tables("tblLogOn").Rows(0).Item("employeeID")
+
+
+        txtEmployeeID.Text = employeeID
+
+        'SQL statement that gets the information on pending holidays
+        sql = "SELECT timeOffStartDate, timeOffEndDate, timeOffReason FROM TIMEOFF where employeeID = " & employeeID & " and approved = no"
+
+        da = New OleDb.OleDbDataAdapter(sql, con)
+        da.Fill(ds, "tblPendHol")
+
+        'populate listbox with pending timeoff data
+        For i = 0 To ds.Tables("tblPendHol").Rows.Count - 1
+            lstBoxPendHol.Items.Add("Reason: " + ds.Tables("tblPendHol").Rows(i).Item("timeOffReason") + "     Start Date: " + ds.Tables("tblPendHol").Rows(i).Item("timeOffStartDate") + "     End Date: " + ds.Tables("tblPendHol").Rows(i).Item("timeOffEndDate"))
+        Next
+
+        'SQL statement that gets the information on pending holidays
+        sql = "SELECT timeOffStartDate, timeOffEndDate, timeOffReason FROM TIMEOFF where employeeID = " & employeeID & " and approved = yes"
+
+        da = New OleDb.OleDbDataAdapter(sql, con)
+        da.Fill(ds, "tblAccHol")
+
+        'populate listbox with accepted timeoff data
+        For i = 0 To ds.Tables("tblAccHol").Rows.Count - 1
+            lstBoxAccHoll.Items.Add("Reason: " + ds.Tables("tblAccHol").Rows(i).Item("timeOffReason") + "     Start Date: " + ds.Tables("tblAccHol").Rows(i).Item("timeOffStartDate") + "     End Date: " + ds.Tables("tblAccHol").Rows(i).Item("timeOffEndDate"))
+        Next
+    End Sub
+
+    ' Contains new time off request forms which allows
+    ' employees to send timeoff requests for admins to review
+    ' Data will be taken from inputs and be sent to the db
+    ' upon clicking "Apply"
+    Private Sub btnAddTimeOff_Click(sender As Object, e As EventArgs) Handles btnAddTimeOff.Click
+        'Creates a new holiday request
+
+        sql = "INSERT INTO TIMEOFF (employeeID, timeOffStartDate, timeOffEndDate, timeOffReason) VALUES ("
+        sql &= "'" & txtEmployeeID.Text & "',"
+        sql &= "'" & dtStart.Value.Date & "',"
+        sql &= "'" & dtEnd.Value.Date & "',"
+        sql &= "'" & txtReason.Text & "')"
+
+        da = New OleDb.OleDbDataAdapter(sql, con)
+        da.Fill(ds, "tblAddHoliday")
+        con.Close()
+
+        MessageBox.Show("Holiday has been sent for approval")
+    End Sub
+
+    Private Sub lstBoxAccHoll_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstBoxAccHoll.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub lstBoxPendHol_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstBoxPendHol.SelectedIndexChanged
+
+    End Sub
+End Class
