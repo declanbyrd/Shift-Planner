a9e7fd789aebd79c3db8bc6f64db5900ef9ccb98
UP807742 <up807742@myport.ac.uk>
2018-03-15 10:45:28 +0000

    UpdatedSQL Query to allow user name to be included

--- Shift-Planner/LoginRegister.vb
+++ Shift-Planner/LoginRegister.vb
@@ -14,7 +14,7 @@ Public Class LoginRegister
             Dim employeeID As Integer = CUInt(txtLUser.Text)
             Dim password As String = txtLPass.Text
 
-            sql = "SELECT employeeID, password, admin FROM EMPLOYEE WHERE employeeID = " & employeeID & " and password = '" + password + "'"
+            sql = "SELECT * FROM EMPLOYEE WHERE employeeID = " & employeeID & " and password = '" + password + "'"
             da = New OleDb.OleDbDataAdapter(sql, con)
             da.Fill(ds, "tblLogOn")
 
@@ -25,26 +25,31 @@ Public Class LoginRegister
                 If ds.Tables("tblLogOn").Rows(0).Item("password") = "changeme" Then
                     If currentAdmin Then
                         currentEmployeeID = ds.Tables("tblLogOn").Rows(0).Item("employeeID")
+                        currentEmployeeName = ds.Tables("tblLogOn").Rows(0).Item("FName") & " " & ds.Tables("tblLogOn").Rows(0).Item("SName")
                         AdminDashboard.Show()
                         AdminDashboard.Enabled = False
                     Else
                         currentEmployeeID = ds.Tables("tblLogOn").Rows(0).Item("employeeID")
+                        currentEmployeeName = ds.Tables("tblLogOn").Rows(0).Item("FName") & " " & ds.Tables("tblLogOn").Rows(0).Item("SName")
                         EmployeeDashboard.Show()
                         EmployeeDashboard.Enabled = False
                     End If
                     firstLogin = True
                     PasswordChange.Show()
                     currentEmployeeID = ds.Tables("tblLogOn").Rows(0).Item("employeeID")
+                    currentEmployeeName = ds.Tables("tblLogOn").Rows(0).Item("FName") & " " & ds.Tables("tblLogOn").Rows(0).Item("SName")
                     MsgBox("Change your password from the default value.")
                     Me.Close()
                 ElseIf currentAdmin Then
                     MessageBox.Show("Log on successful as admin.")
                     currentEmployeeID = ds.Tables("tblLogOn").Rows(0).Item("employeeID")
+                    currentEmployeeName = ds.Tables("tblLogOn").Rows(0).Item("FName") & " " & ds.Tables("tblLogOn").Rows(0).Item("SName")
                     AdminDashboard.Show()
                     Me.Close()
                 Else
                     MessageBox.Show("Log on successful as employee.")
                     currentEmployeeID = ds.Tables("tblLogOn").Rows(0).Item("employeeID")
+                    currentEmployeeName = ds.Tables("tblLogOn").Rows(0).Item("FName") & " " & ds.Tables("tblLogOn").Rows(0).Item("SName")
                     EmployeeDashboard.Show()
                     Me.Close()
 
