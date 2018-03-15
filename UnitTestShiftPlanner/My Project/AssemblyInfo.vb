4b4e6753fea9a042b46297d939f4bd8620079a40
UP807742 <up807742@myport.ac.uk>
2018-03-15 19:33:51 +0000

    Log in unit tests

---
+++ UnitTestShiftPlanner/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("UnitTestShiftPlanner")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("UnitTestShiftPlanner")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("c55a0f23-e06e-43aa-ba2f-15b5f23b0b23")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ UnitTestShiftPlanner/UnitTest.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports Shift_Planner
+
+<TestClass()> Public Class UnitTest
+
+    <TestMethod()> Public Sub correctLogon()
+        Dim employeeID As Integer = 1
+        Dim password As String = "password"
+
+        Assert.IsTrue(LoginRegister.Signin(employeeID, password), "The user has successfully logged on")
+
+    End Sub
+
+    <TestMethod()> Public Sub wrongPassword()
+        Dim employeeID As Integer = 1
+        Dim password As String = "My First Pet"
+
+        Assert.IsTrue(LoginRegister.Signin(employeeID, password), "The password is incorrect and should not let the user log in")
+
+    End Sub
+    <TestMethod()> Public Sub wrongUsername()
+        Dim employeeID As Integer = 99
+        Dim password As String = "password"
+
+        Assert.IsTrue(LoginRegister.Signin(employeeID, password), "The employee ID should be invalid and the user will not be logged in")
+
+    End Sub
+
+    <TestMethod()> Public Sub usernameAsString()
+        Dim employeeID As Integer = "Rhubarb Bikini"
+        Dim password As String = "password"
+
+        Assert.IsTrue(LoginRegister.Signin(employeeID, password), "The employee ID should be invalid and the user will not be logged in")
+
+    End Sub
+
+End Class
